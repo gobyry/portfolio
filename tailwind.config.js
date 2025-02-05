8537c2b26f4cecb496968b6aec1966ee41ed3c49
Ryan Hui <ryanhui30@gmail.com>
2025-02-05 11:59:04 -0800

    Created project. Made progress to Navbar and App.jsx

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx,html}"], // Make sure this matches your file structure
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
